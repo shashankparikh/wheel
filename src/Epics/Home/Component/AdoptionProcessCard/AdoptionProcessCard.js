8e56c58778f4f7972830fcafdf98ad509777a501
Ankit Parikh <ankit@iicreators.com>
2019-11-07 00:52:27 +0530

    Flex and Styled Components

--- src/Epics/Home/Component/AdoptionProcessCard/AdoptionProcessCard.js
+++ src/Epics/Home/Component/AdoptionProcessCard/AdoptionProcessCard.js
@@ -3,17 +3,27 @@ import { Text, View } from 'react-native'
 import styled from 'styled-components/native'
 
 const StyledView = styled.View`
-  background-color: #fff6d7;
-  height: 200px;
-  box-shadow: 10px 5px 5px black;
-  width:40%;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff6d7;
+    height: 150px;
+    box-shadow: 10px 5px 5px black;
+    width:40%;
+`
+
+const Title = styled.Text`
+    font-size: 24px;
+    font-weight: 500;
+    color: palevioletred;
 `
 
 export class AdoptionProcessCard extends Component {
     render() {
         return (
             <StyledView>
-                <Text> {this.props.title} </Text>
+                <Title> {this.props.title} </Title>
             </StyledView>
         )
     }
